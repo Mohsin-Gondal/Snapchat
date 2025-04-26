7993949208d45412c2f8e0ff3e0995b0d896f4a3
Mohsin Raza <mohsin.debug@gmail.com>
2025-04-26 21:52:26 +0500

    Final Commit

--- controller/MailController.js
+++ controller/MailController.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer');
+require('dotenv').config();
 
 // Create a transporter object using the default SMTP transport
 const transporter = nodemailer.createTransport({
   service: 'gmail', // or use another service like 'smtp.mailtrap.io', etc.
   auth: {
-    user: '23014198165.uog.edu.pk@gmail.com', // Your email address
-    pass: 'mlqu uoax iotw akkv',  // Your email password or app-specific password (for Gmail)
+    user: process.env.USER, // Your email address
+    pass: process.env.PASS,  // Your email password or app-specific password (for Gmail)
   },
 });
 module.exports = transporter;
